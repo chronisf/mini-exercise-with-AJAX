5477e8e8ce30b17ffbdb9da4d5f5909cfb9b2fda
Chronis Fiflis <76593968+chronisf@users.noreply.github.com>
2021-01-15 16:13:46 +0200

    Add files via upload

---
+++ php/transfer.php
@@ -0,0 +1,7 @@
+<?php
+ $n=$_GET["name"];
+ $l=$_GET["lname"];
+ $e=$_GET["email"];
+echo " Τα στοιχεία που πέρασαν στο αρχείο data.php είναι:<br>";
+echo"Όνομα $n- Επίθετο $l-  email $e";
+?>
---
+++ php/transfer2.php
@@ -0,0 +1,7 @@
+<?php
+ $n=$_POST["name"];
+ $l=$_POST["lname"];
+ $e=$_POST["email"];
+echo " Τα στοιχεία που πέρασαν στο αρχείο data.php είναι:<br>";
+echo"Όνομα $n- Επίθετο $l-  email $e";
+?>
